b4e2cab64218c2e060901b534ed7bf59aedf8807
gmb <gameboyt7@gmail.com>
2022-08-22 18:35:47 +0200

    Updated install and run scripts to include joberty

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd api-gateway && npm run lint && cd -
-cd microservices/auth && npm run lint && cd -
-cd microservices/user && npm run lint && cd -
-cd microservices/post && npm run lint && cd -
+cd api-gateway && npm run lint && cd - &&
+cd microservices/auth && npm run lint && cd - &&
+cd microservices/user && npm run lint && cd - &&
+cd microservices/post && npm run lint && cd - &&
+cd joberty && npm run lint && cd - 
