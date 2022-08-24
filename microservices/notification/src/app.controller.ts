831a534e806c115d0f08eb1c71cb62ed49e61ffb
gmb <gameboyt7@gmail.com>
2022-08-24 19:37:04 +0200

    Added notification microservice skeleton

---
+++ microservices/notification/src/app.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Inject, Logger, Sse } from '@nestjs/common';
+import { AppService } from './app.service';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {
+  ClientProxy,
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Sse('sse')
+  sse() {
+    return interval(1000).pipe(map((_) => ({ data: { hello: 'world' } })));
+  }
+
+  @EventPattern('post_liked')
+  async handlePostLiked(payload) {
+    this.logger.log('handlePostLiked.event#payload', payload);
+  }
+}
