09aee11bf0fffc6b2862d7d6821f7c97e97c5aa5
gmb <gameboyt7@gmail.com>
2022-08-31 15:19:49 +0200

    Added user notifications

--- dislikt-react-app/.eslintrc.js
+++ dislikt-react-app/.eslintrc.js
@@ -13,12 +13,18 @@ module.exports = {
 		'ecmaVersion': 'latest',
 		'sourceType': 'module'
 	},
+	settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 	'plugins': [
 		'react'
 	],
 	'rules': {
 		'react/react-in-jsx-scope': 'off',
 		"react/no-unescaped-entities": "off",
+		"react/prop-types": 'off',
 		"no-unused-vars": "off",
 	}
 }
--- dislikt-react-app/src/index.js
+++ dislikt-react-app/src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+axios.interceptors.request.use(function (config) {
+  const loggedInUser = JSON.parse(window.localStorage.getItem('loggedInUser'))
+  // if (loggedInUser) {
+    // const token = `Bearer ${loggedInUser.accessToken}`
+    const token = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiN2FmZGFiZmQtOGIyZi00MDQ3LWIxMmEtMjE0YTA2MTgzNTBiIiwiZW1haWwiOiJlbWFAZ2EuY29tIiwidXNlcm5hbWUiOiJ1c2VyIiwicGFzc3dvcmQiOiIkMmIkMTAkaVU2SThMRGtoZml3OGJxbGtkUEdtdVU5ajR4dDBkSzRqbXYuOVhDY2tUbnRMWVF2M2xoV3EifSwic3ViIjoiN2FmZGFiZmQtOGIyZi00MDQ3LWIxMmEtMjE0YTA2MTgzNTBiIiwiaWF0IjoxNjYxOTUwMDc5LCJleHAiOjE2NjQ1NDIwNzl9.xIHZqeKXshgaTK0dBZu31KdbHvlQgJ8XaEK7TwP_F5o`
+    config.headers['Authorization'] = token
+  // }
+  return config
+})
+
 root.render(
   <App />
 );
